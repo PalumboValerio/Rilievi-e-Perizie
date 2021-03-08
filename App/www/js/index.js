dd857dc1d9b5ea8f2423356ff2ee4c8ff8017cc1
PalumboValerio <v.palumbo.1004@vallauri.edu>
2021-03-08 22:30:24 +0100

    Starting app

--- App/www/js/index.js
+++ App/www/js/index.js
@@ -1,17 +1,27 @@
-$(document).ready(function() {
-    document.addEventListener('deviceready', function() 
-    {
-        let _wrapper =$("#wrapper");
-        let cameraOptions={"quality":50};
+$(document).ready(function () {
+    document.addEventListener('deviceready', function () {
+        let form=$("#modal");
+        let txtEmail=$("#input_5");
+        let txtCoords=$("#input_9");
+        let txtDateTime=$("#input_11");
+        let txtNotes=$("#input_8");
+
+        let cameraOptions = {
+            "quality": 50
+        };
         let gpsOptions = {
             enableHighAccuracy: true,
             timeout: 5000,
             maximumAge: 0
         };
-        
-        $("#btnScatta").on("click", function(){
-            cameraOptions.sourceType=Camera.PictureSourceType.CAMERA;
-            cameraOptions.destinationType=Camera.DestinationType.DATA_URL;
+
+        $(".close").on("click", function(){
+            form.css("display", "none");
+        })
+
+        $("#btnPhoto").on("click", function () {
+            cameraOptions.sourceType = Camera.PictureSourceType.CAMERA;
+            cameraOptions.destinationType = Camera.DestinationType.DATA_URL;
             navigator.camera.getPicture(success, error, cameraOptions);
         });
 
@@ -21,36 +31,38 @@ $(document).ready(function() {
             navigator.camera.getPicture(success, error, cameraOptions);
         });*/
 
-        function success(image)
-        {
-            $("<img>").css("height", 80).prop("src", `data:image/jpeg;base64,${image}`).appendTo(_wrapper);
-            let date=new Date();
+        function success(image) {
+            $("#img_14").prop("src", `data:image/jpeg;base64,${image}`);
+            let date = new Date();
             // Codice/Mail operatore da reperire dal db. Dopo il login riuscito, salvarsi la mail
             // Note mediante una comparsa di una form che conterrà preview della foto scattata, 
             // posizione, data e ora e una input box per un eventuale commento facoltativo
-            navigator.geolocation.getCurrentPosition(function(){
+            navigator.geolocation.getCurrentPosition(function (pos) {
                 // Appare la form con la conferma dell'inserimento. Premendo su conferma si chiama il
                 // server e gli si passano i dati per inserire nel db la nuova perizia
+                txtEmail.val("prova@syphon.com");
+                txtCoords.val(`Lat: ${pos.coords.latitude} - Lon: ${pos.coords.longitude}`);
+                txtDateTime.val(date.toUTCString());
+
+                form.css("display", "block");
             }, error, gpsOptions);
         }
 
-        function error(err)
-        {
-            if(err.code)
-            {
+        function error(err) {
+            if (err.code) {
                 notifica(`Errore: ${err.code}-${err.message}`);
             }
         }
 
-        function notifica(msg){		 
+        function notifica(msg) {
             navigator.notification.alert(
-                msg,    
-                voidFunction,       
-                "Info",       // Titolo finestra
-                "Ok"          // pulsante di chiusura
-            );			 
+                msg,
+                voidFunction,
+                "Info", // Titolo finestra
+                "Ok" // pulsante di chiusura
+            );
         }
 
-        let voidFunction=function() {}
+        let voidFunction = function () {}
     });
-});    
+});
